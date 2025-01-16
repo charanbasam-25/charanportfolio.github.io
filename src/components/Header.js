53948ec99d2a881085dd5905c1f7e695ecae48d0
charanbasam-25 <be.charankumar@gmail.com>
2025-01-16 22:17:17 +0530

    header colour chnage

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     const params = useParams()
     console.log(params, 'params----')
   return (
-    <header className='fixed top-0 w-full z-10 bg-gradient-to-r from-blue-500 to-green-500'>
+    <header className='fixed top-0 w-full z-10 bg-gradient-to-r from-gray-100 to-gray-300'>
       <nav className='header-component grid grid-flow-col items-center py-4 px-6 font-sans font-semibold h-16 shadow-lg'>
         <Link to="/" className='flex items-center lg:px-20'>
           <span className='logo col-span-2'>
@@ -16,9 +16,9 @@ const Header = () => {
           </span>
         </Link>
         <ul className="all-routes flex justify-end items-center lg:px-20 space-x-6">
-            <Link to="/"><li className="text-gray-800 hover:text-yellow-400 transition-all duration-300 px-4 py-2">Home</li></Link>
-            <Link to='/about'><li className="text-gray-800 hover:text-yellow-400 transition-all duration-300 px-4 py-2">About</li></Link>
-            <Link to="/projects"><li className="text-gray-800 hover:text-yellow-400 transition-all duration-300 px-4 py-2">Projects</li></Link>            
+            <Link to="/"><li className="text-gray-800 hover:text-black transition-all duration-300 px-4 py-2">Home</li></Link>
+            <Link to='/about'><li className="text-gray-800 hover:text-black transition-all duration-300 px-4 py-2">About</li></Link>
+            <Link to="/projects"><li className="text-gray-800 hover:text-black transition-all duration-300 px-4 py-2">Projects</li></Link>            
         </ul>
       </nav>
     </header>
--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
     <div className="home bg-gradient-to-b from-gray-800 via-gray-900 to-black text-white p-20 h-screen flex flex-col justify-center items-center" id="home">
       <div className="flex justify-center items-center flex-col md:flex-row">
         {/* Profile Image Section */}
-        <div className="rounded-full border-4 border-indigo-500 p-2 md:h-56 md:w-56 h-40 w-40 hover:scale-110 transform transition-transform duration-300">
+        <div className="rounded-full border-4 border-gray-500 p-2 md:h-56 md:w-56 h-40 w-40 hover:scale-110 transform transition-transform duration-300">
           <img
             alt="profile"
             loading="lazy"
@@ -21,9 +21,9 @@ const Home = () => {
         </div>
         {/* Intro Section */}
         <div className="text-center md:text-left md:ml-10 mt-6 md:mt-0">
-          <h1 className="text-5xl font-extrabold text-indigo-400">Charan Kumar</h1>
-          <h2 className="text-3xl font-semibold text-indigo-300 mt-2">Web Developer</h2>
-          <h3 className="text-xl font-medium mt-2 bg-clip-text text-transparent bg-gradient-to-r from-indigo-300 via-green-400 to-green-500">
+          <h1 className="text-5xl font-extrabold text-gray-200">Charan Kumar</h1>
+          <h2 className="text-3xl font-semibold text-gray-200 mt-2">Web Developer</h2>
+          <h3 className="text-xl font-medium mt-2 bg-clip-text text-transparent bg-gradient-to-r from-gray-300 via-gray-400 to-black">
             Building Better Web
           </h3>
         </div>
@@ -31,7 +31,7 @@ const Home = () => {
         {/* Resume & Contact Buttons */}
       <div className="mt-12 flex flex-col md:flex-row items-center">
         <a
-          className="px-8 py-4 mx-2 bg-indigo-500 text-white font-medium rounded-lg shadow-md hover:bg-indigo-600 transform hover:scale-105 transition-transform duration-300"
+          className="px-8 py-4 mx-2 bg-gray-300 text-black font-medium rounded-lg shadow-md hover:bg-white transform hover:scale-105 transition-transform duration-300"
           href="https://drive.google.com/file/d/14ThTD1RQxyA7biehfnuvdRw2qohZqjK-/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
@@ -44,7 +44,7 @@ const Home = () => {
           />
         </a>
         <div
-          className="px-8 py-4 mx-2 bg-indigo-500 text-white font-medium rounded-lg shadow-md flex items-center cursor-pointer hover:bg-indigo-600 transform hover:scale-105 transition-transform duration-300"
+          className="px-8 py-4 mx-2 bg-gray-300 text-black font-medium rounded-lg shadow-md flex items-center cursor-pointer hover:bg-white transform hover:scale-105 transition-transform duration-300"
           onMouseEnter={handleTooltipToggle}
           onMouseLeave={handleTooltipToggle}
           style={{ position: 'relative' }}
